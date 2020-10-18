b10eef04588cc426fcff55475fed4fad91dca366
andicodetrf <andrea_lau92@hotmail.com>
2020-10-18 12:37:18 +0800

    test api connection without db

--- src/controllers/HealthcheckController.js
+++ src/controllers/HealthcheckController.js
@@ -1,28 +1,39 @@
 import Express from 'express';
 import { OK } from 'http-status-codes';
-const db = require('../config/db.config');
-const Teacher = db.teacher;
+// const db = require('../config/db.config');
+// const Teacher = db.teacher;
 import { errHandler } from '../utils/index'
 
 
 const HealthcheckController = Express.Router();
 
+// const healthcheckHandler = async (req, res) => {
+//   try{
+
+//     let foundTeachers = await Teacher.findAll()
+//     if(foundTeachers){
+//       // console.log(foundTeachers)
+//       return res.status(OK).json({
+//         status: OK,
+//         foundTeachers
+//       })
+//     }
+//   } catch (err){
+//     errHandler(err)
+//   }
+
+// }
+
+
 const healthcheckHandler = async (req, res) => {
   try{
-
-    let foundTeachers = await Teacher.findAll()
-    if(foundTeachers){
-      // console.log(foundTeachers)
-      return res.status(OK).json({
-        status: OK,
-        foundTeachers
-      })
-    }
+    return res.status(OK).json({
+      status: OK,
+    })
   } catch (err){
     errHandler(err)
   }
 }
-
 
 HealthcheckController.get('/healthcheck', healthcheckHandler);
 
